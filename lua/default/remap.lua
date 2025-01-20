efe669002497d0d99fcabe327c83bd134c661534
Greve2001 <frederik.greve@gmail.com>
2025-01-20 13:56:27 +0100

    Init

---
+++ lua/default/remap.lua
@@ -0,0 +1,21 @@
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- Project View
+
+-- ALT moving
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keeping cursor in middle when searching
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Paste without adding to buffer
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Delete without adding to buffer
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- Copy to clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
